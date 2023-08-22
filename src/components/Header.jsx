a826f3db0959fae41a35017b31d96a3dbb70487a
ayashadevikar <ayashadevikar8@gmail.com>
2023-08-22 22:01:52 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <nav>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+         <a href="#">Home</a>
+     </nav>
+  ) 
+}
+
+export default Header
