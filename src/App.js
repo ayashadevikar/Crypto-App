63f4dd5283565663ef0aa3dbaf11596579cf864d
ayashadevikar <ayashadevikar8@gmail.com>
2023-08-23 23:45:42 +0530

    exchanges component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
  import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
 
 function App() {
   return (
@@ -8,11 +11,14 @@ function App() {
         <Header />
           <Routes>
                <Route path="/" element={<Home />}/>
-               <Route path="/" element={<Home />}/>
-               <Route path="/" element={<Home />}/>
+               <Route path="/coins" element={<Coins />}/>
+               <Route path="/exchanges" element={<Exchanges />}/>
+               <Route path="/coin/:id" element={<CoinDetails />}/>
           </Routes>
      </Router>
   );
 }
+
+
 
 export default App;
--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import { server } from "../index";
 
 const Exchanges = () => {
+   
+  const [exchanges,setExchanges] = useState([]);
+
+    useEffect(() => {
+        const fetchExchanges = async () => {
+            const { data } = await axios.get(`${server}/exchanges`);
+           console.log(data);
+           setExchanges(data);
+        }
+        fetchExchanges();
+    },[])
+
   return (
-    <div>
-      Exchanges
-    </div>
+     <>
+       {
+        exchanges.map((i) => (
+          <exchanges key={i.id} />
+        )
+            
+        )
+       }
+        
+     </>
   )
 }
 
